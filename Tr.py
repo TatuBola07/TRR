1283f769a1ca122bba008f1833bdb0f852d0aea1
TatuBola07 <106366285+TatuBola07@users.noreply.github.com>
2022-05-31 00:20:57 -0300

    Update Tr.py

--- Tr.py
+++ Tr.py
@@ -5,22 +5,49 @@ from Crypto.Hash import keccak
 from tronpy.keys import PrivateKey
 import colorama
 from colorama import Fore, Back, Style
-from bitcoinaddress import Wallet
+
 colorama.init()
 
 
 print("Please Wait...")
 
 a = 1
-b = 2
-c = 100
-count=0
-remaining=c-b
-F = []
-P = b
-while P<c:
-    count+= 1
-    ran= P
-    HEX = "%064x" % ran
-    priv_key = PrivateKey(HEX)
-    print(priv_key)
+while True:
+
+    priv_key ='da146374a75310b9666e834ee4ad0866d6f4035967bfc76217c5a495fff9f0d0'
+    xbase = priv_key.public_key.to_base58check_address()
+    xHex = priv_key.public_key.to_hex_address()
+    Prix = priv_key.hex()
+    xPub = priv_key.public_key.hex()
+    bloc = requests.get("https://apilist.tronscan.org/api/account?address=" + xbase)
+    res = bloc.json()
+    balances = dict(res)["balance"]
+    transaction = dict(res)["totalTransactionCount"]
+    frozen = dict(res)["totalFrozen"]
+
+    print("\n\n===========================")
+    print(str(a) + " --> Account information ")
+    print("---------------------------")
+    print(Fore.YELLOW + "Address :" + xbase + " " + Style.RESET_ALL)
+    print("---------------------------")
+    print(Fore.LIGHTRED_EX + "Hex :", xHex + " " + Style.RESET_ALL)
+    print("---------------------------")
+    print(Fore.LIGHTWHITE_EX + "Private Key :", Prix + " " + Style.RESET_ALL)
+    print("---------------------------")
+    print(Fore.LIGHTGREEN_EX + "Public Address :", xPub + " " + Style.RESET_ALL)
+    print("---------------------------")
+    print(
+        Fore.RED
+        + "[ "
+        + " Balance = "
+        + str(balances)
+        + " | "
+        + "  Frozen  = "
+        + str(frozen)
+        + " | "
+        + "  Transactions = "
+        + str(transaction)
+        + " ]"
+        + Style.RESET_ALL
+    )
+    a = a + 1

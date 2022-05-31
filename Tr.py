a9fd869588419ddac1305fc76e43f8cd33987682
TatuBola07 <106366285+TatuBola07@users.noreply.github.com>
2022-05-31 01:58:24 -0300

    Update Tr.py

--- Tr.py
+++ Tr.py
@@ -1,53 +1,67 @@
-import ecdsa
-import base58
+import bit
+from bit import *
+from bit.format import bytes_to_wif
+import hashlib
+from bitcoinlib.encoding import pubkeyhash_to_addr_bech32, addr_bech32_to_pubkeyhash, change_base
+
+import json
+import random
 import requests
-from Crypto.Hash import keccak
-from tronpy.keys import PrivateKey
-import colorama
-from colorama import Fore, Back, Style
+from lxml import html
+from rich import print
+from rich.panel import Panel
+from hdwallet import HDWallet
+from rich.console import Console
+from hdwallet.symbols import TRX as trx
 
-colorama.init()
+console = Console(safe_box="None")
 
 
-print("Please Wait...")
+def HASH160(pubk_bytes):
+	return hashlib.new('ripemd160', hashlib.sha256(pubk_bytes).digest()).digest()
 
-a = 1
-while True:
 
-    priv_key ='da146374a75310b9666e834ee4ad0866d6f4035967bfc76217c5a495fff9f0d0'
-    xbase = priv_key.public_key.to_base58check_address()
-    xHex = priv_key.public_key.to_hex_address()
-    Prix = priv_key.hex()
-    xPub = priv_key.public_key.hex()
-    bloc = requests.get("https://apilist.tronscan.org/api/account?address=" + xbase)
-    res = bloc.json()
-    balances = dict(res)["balance"]
-    transaction = dict(res)["totalTransactionCount"]
-    frozen = dict(res)["totalFrozen"]
+x = 1
+y = 10000
+F = []
+P = x
 
-    print("\n\n===========================")
-    print(str(a) + " --> Account information ")
-    print("---------------------------")
-    print(Fore.YELLOW + "Address :" + xbase + " " + Style.RESET_ALL)
-    print("---------------------------")
-    print(Fore.LIGHTRED_EX + "Hex :", xHex + " " + Style.RESET_ALL)
-    print("---------------------------")
-    print(Fore.LIGHTWHITE_EX + "Private Key :", Prix + " " + Style.RESET_ALL)
-    print("---------------------------")
-    print(Fore.LIGHTGREEN_EX + "Public Address :", xPub + " " + Style.RESET_ALL)
-    print("---------------------------")
-    print(
-        Fore.RED
-        + "[ "
-        + " Balance = "
-        + str(balances)
-        + " | "
-        + "  Frozen  = "
-        + str(frozen)
-        + " | "
-        + "  Transactions = "
-        + str(transaction)
-        + " ]"
-        + Style.RESET_ALL
-    )
-    a = a + 1
+
+def trxbal(trx_addr):
+	block = requests.get("https://apilist.tronscan.org/api/account?address=" + trx_addr)
+	res = block.json()
+	balances = dict(res)["balances"][0]["amount"]
+	return balances
+
+
+z = 1
+w = 0
+while P < y:
+	P += 1
+	ran = P
+	pk = Key.from_int(ran)
+	key = Key.from_int(ran)
+	hasher = key.to_hex()
+	PRIVATE_KEY = str(hasher)
+	trx1: HDWallet = HDWallet(symbol=trx)
+	trx1.from_private_key(private_key=PRIVATE_KEY)
+	trx_addr = trx1.p2pkh_address()
+	trxbalx = trxbal(trx_addr)
+	amount_trx = '0.000000'
+	if str(trxbalx) != str(amount_trx):
+		console.print(
+			'[white on green]TRX Address: [white on red1]' + str(trx_addr) + '[/][gold1 on black]   BALANCE: [white]' +
+			str(trxbalx) + '[/]\n[blue]KEY: ' + str(PRIVATE_KEY) + '[/]')
+		z += 1
+		w += 1
+		f = open("TrxNewWiner.txt", "a")
+		f.write('\n' + str(trx_addr) + '     BALANCE:' + str(trxbalx) + '\nPrivateKey:' + str(PRIVATE_KEY) + '\n-----------------[ '
+		                                                                                                     'Mmdrza.Com '
+		                                                                                                     ']-------------------')
+		f.close()
+	
+	
+	else:
+		console.print(str(z) + '[gold1]  Win:[white]' + str(w) + '[gold1]  TRX Address: [red1]' + str(trx_addr) + '[/][gold1]   BALANCE: [white]' + str(
+			trxbalx) + '[/]')
+		z += 1
